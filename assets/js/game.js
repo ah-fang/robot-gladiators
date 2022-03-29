6c5b316591384d2b9b3fe459eb910ef8869743e7
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-03-28 17:18:06 -0700

    initial files

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+
+function fight() {
+    window.alert("The fight has begun!");
+} 
+
+var playerName = window.prompt("What is your robot's name?");
+console.log("Our robot's name is" + playerName);
+// fight();
+var name = "your name";
+console.log(name); 
